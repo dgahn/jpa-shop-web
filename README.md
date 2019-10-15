3d3d948ae2d0a55d2f75c2fb054b7d05f122cd80
dgAhn <dgAhn@icloud.com>
2019-10-15 13:35:46 +0900

    프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,18 @@
+# JPA SHOP
+
+## Overview
+
+이 저장소는 김영한님의 [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-JPA-%ED%99%9C%EC%9A%A9-1#) 코드 저장소입니다.
+ 코드에 대한 설명은 강의를 참고해주시기 바랍니다.
+
+## building
+
+```bash
+$ ./gradlew build
+```
+
+## run
+
+```bash
+$ ./gradlew run
+```
---
+++ src/main/java/io/github/dgahn/jpashop/JpashopApplication.java
@@ -0,0 +1,14 @@
+package io.github.dgahn.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JpashopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpashopApplication.class, args);
+  }
+
+}
